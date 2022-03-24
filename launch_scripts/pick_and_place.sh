9a838cc48931a69347880f081fde0246b75e786d
aurel <appiusa@student.ethz.ch>
2022-03-24 15:51:02 +0100

    made shell script functional

--- launch_scripts/pick_and_place.sh
+++ launch_scripts/pick_and_place.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo 'Starting pick and place' 
-bash -x ./../build/modules/mocap_publisher/app/mocap_publisher
+gnome-terminal -- ./build/modules/mocap_publisher/app/mocap_publisher 
+gnome-terminal -- ./build/modules/position_ctrl_interface/apps/position_control/pos_ctrl_interface serial:///dev/ttyACM0
+gnome-terminal -- ./build/modules/reference_generator/apps/pick_and_place/pick_and_place 
