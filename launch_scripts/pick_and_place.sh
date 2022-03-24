8585321e9be59f035c6f921bea1f8645612feb14
aurel <appiusa@student.ethz.ch>
2022-03-24 15:54:33 +0100

    improved shell script

--- launch_scripts/pick_and_place.sh
+++ launch_scripts/pick_and_place.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo 'Starting pick and place' 
-gnome-terminal -- ./build/modules/mocap_publisher/app/mocap_publisher 
+echo '*** PICK AND PLACE MISSION ***' 
+gnome-terminal -- ./build/modules/mocap_publisher/app/mocap_publisher srl_quad srl_stand srl_pick_crate srl_place_crate
+echo 'Started mocap publisher' 
 gnome-terminal -- ./build/modules/position_ctrl_interface/apps/position_control/pos_ctrl_interface serial:///dev/ttyACM0
+echo 'Started Position Control Interface' 
 gnome-terminal -- ./build/modules/reference_generator/apps/pick_and_place/pick_and_place 
+echo 'Started Reference Generator' 
